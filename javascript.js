3444de673faf9b3c74526d7fa702867129383bef
aeleby <ae.mae.aw@gmail.com>
2022-09-25 13:00:05 -0700

    add edits and project to git

---
+++ javascript.js
@@ -0,0 +1,36 @@
+let num1 = document.getElementById("num1");
+
+let count = 12;
+
+function score1() {
+  count = count + 1;
+  num1.textContent = count;
+}
+
+function score2() {
+  count = count + 2;
+  num1.textContent = count;
+}
+
+function score3() {
+  count = count + 3;
+  num1.textContent = count;
+}
+
+let num2 = document.getElementById("num2");
+let count1 = 5;
+
+function score11() {
+  count1 = count1 + 1;
+  num2.textContent = count1;
+}
+
+function score21() {
+  count1 = count1 + 2;
+  num2.textContent = count1;
+}
+
+function score31() {
+  count1 = count1 + 3;
+  num2.textContent = count1;
+}
